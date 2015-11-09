cc2ea2d79b1efc9e6eec35012d16fde38c2ba338
Karl Augustsson <karl.augustsson@gmail.com>
2015-11-09 21:24:49 +0100

    desciding to still not use real mail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,9 +80,11 @@ Rails.application.configure do
 
     config.action_mailer.delivery_method = :smtp
 config.action_mailer.smtp_settings = {
-  :host => '46.101.15.72'
-
-
-
+  :user_name => '429182d2f7ef62c15',
+  :password => '82cfb3c20eb52f',
+  :address => 'mailtrap.io',
+  :domain => 'mailtrap.io',
+  :port => '2525',
+  :authentication => :cram_md5
 }
 end
